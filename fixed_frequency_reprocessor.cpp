68a60147d8c2278642f9eb9bd43c2d167cf24552
cuppajoeman <ccn@cuppajoeman.com>
2025-01-25 21:53:17 -0500

    feat: initial logic

---
+++ fixed_frequency_reprocessor.cpp
@@ -0,0 +1,60 @@
+#include "fixed_frequency_reprocessor.hpp"
+
+FixedFrequencyReprocessor::FixedFrequencyReprocessor(int frequency, std::function<void(int, double)> fun,
+                                                     std::function<void(int)> re_fun)
+    : process_fun(fun), reprocess_fun(re_fun), periodic_signal(frequency) {
+    // Compute the period from frequency (time interval between each processing cycle)
+    period = 1.0 / frequency;
+}
+
+bool FixedFrequencyReprocessor::attempt_to_process() {
+
+    if (periodic_signal.process_and_get_signal()) {
+
+        for (int id : ids_to_process) {
+            double dt = periodic_signal.get_last_delta_time();
+            id_to_delta_time[id] = dt;
+            process_fun(id, dt);
+            processed_ids.push_back(id);
+        }
+        ids_to_process.clear();
+        return true;
+    }
+    return false;
+}
+
+void FixedFrequencyReprocessor::add_id(int id) {
+    std::cout << "Adding ID: " << id << std::endl;
+    ids_to_process.push_back(id);
+}
+
+void FixedFrequencyReprocessor::display_processed_ids() const {
+    std::cout << "Processed IDs: ";
+    for (const auto &id : processed_ids) {
+        std::cout << id << " ";
+    }
+    std::cout << std::endl;
+}
+
+std::vector<int> FixedFrequencyReprocessor::get_ids_processed_after(int id) {
+    std::vector<int> ids_after;
+    for (const auto &processed_id : processed_ids) {
+        if (processed_id > id) {
+            ids_after.push_back(processed_id);
+        }
+    }
+    return ids_after;
+}
+
+void FixedFrequencyReprocessor::re_process_after_id(int id) {
+    std::cout << "Reprocessing activated after ID: " << id << std::endl;
+
+    reprocess_fun(id);
+
+    for (const auto &processed_id : processed_ids) {
+        if (processed_id > id) {
+            process_fun(processed_id, id_to_delta_time[processed_id]);
+        }
+    }
+    std::cout << "Reprocessing done" << std::endl;
+}
---
+++ fixed_frequency_reprocessor.hpp
@@ -0,0 +1,56 @@
+#ifndef FIXED_FREQUENCY_REPROCESSOR_HPP
+#define FIXED_FREQUENCY_REPROCESSOR_HPP
+
+#include <vector>
+#include <functional>
+#include <iostream>
+#include "../periodic_signal/periodic_signal.hpp"
+
+/**
+ * @brief A class for processing and reprocessing IDs at a fixed frequency.
+ */
+class FixedFrequencyReprocessor {
+  private:
+    std::vector<int> ids_to_process; ///< Vector of IDs that need to be processed.
+    double period;                   ///< Time period (in seconds) between processing cycles, computed from frequency.
+    std::unordered_map<int, double> id_to_delta_time;
+    std::function<void(int, double)> process_fun; ///< Function to process each ID.
+    std::function<void(int)> reprocess_fun;       ///< Function to reprocess IDs after a specific ID.
+    PeriodicSignal periodic_signal;
+
+  public:
+    std::vector<int> processed_ids; ///< Vector of IDs that have been processed.
+    /**
+     * @brief Constructs a FixedFrequencyReprocessor.
+     * @param frequency The number of times the process should happen per second.
+     * @param fun Function to process each ID.
+     * @param re_fun Function to reprocess IDs after a specific ID.
+     */
+    FixedFrequencyReprocessor(int frequency, std::function<void(int, double)> fun, std::function<void(int)> re_fun);
+
+    /**
+     * @brief Adds an ID to the list of IDs to be processed.
+     * @param id The ID to add.
+     */
+    void add_id(int id);
+
+    /**
+     * @brief Attempts to process IDs if the accumulated time exceeds the processing interval.
+     */
+    bool attempt_to_process();
+
+    /**
+     * @brief Displays the list of processed IDs.
+     */
+    void display_processed_ids() const;
+
+    /**
+     * @brief Reprocesses IDs after a specified ID.
+     * @param id The ID after which reprocessing starts.
+     */
+    void re_process_after_id(int id);
+
+    std::vector<int> get_ids_processed_after(int id);
+};
+
+#endif // FIXED_FREQUENCY_REPROCESSOR_HPP
